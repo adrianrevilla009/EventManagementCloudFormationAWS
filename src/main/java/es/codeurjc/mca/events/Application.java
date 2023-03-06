e2218530d022a5131ed123b5d416c24ddec41dfd
adrianrevilla009 <adrianrg1996@gmail.com>
2023-03-06 16:19:50 +0100

    spring cloud for rds

--- src/main/java/es/codeurjc/mca/events/Application.java
+++ src/main/java/es/codeurjc/mca/events/Application.java
@@ -2,10 +2,12 @@ package es.codeurjc.mca.events;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
